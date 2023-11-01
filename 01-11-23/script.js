a44396268bf3b650336cc09d005d8157a0a4824a
olgatobarbato <148492216+olgatobarbato@users.noreply.github.com>
2023-11-01 11:24:28 +0100

    Esercitazione 01-11-23

---
+++ 01-11-23/script.js
@@ -0,0 +1,15 @@
+const animals = [
+  "cat",
+  "dog",
+  "cow",
+  "shark",
+  "lion",
+  "dolphin",
+  "turtle",
+  "eagle",
+];
+console.log(animals);
+
+for (let i = 0; i <= animals.length - 1; i++) {
+  console.log(i + ":", animals[i]);
+}
