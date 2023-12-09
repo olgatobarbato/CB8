cda4c7e75a50ba554fdde6914165948eea285be2
olgatobarbato <148492216+olgatobarbato@users.noreply.github.com>
2023-12-09 16:27:58 +0100

    Delete CodeWeekVanillaJS directory

--- CodeWeekVanillaJS/modules/other.js
+++
@@ -1,9 +0,0 @@
-const associateGenresWithMovies = (genres, movies) => {
-  return genres.map((genre) => ({
-    id: genre.id,
-    name: genre.name,
-    movies: movies.filter((movie) => movie.genre_ids.includes(genre.id)),
-  }));
-};
-
-export { associateGenresWithMovies };
